51eebc9b5990eeff771bfb9635026e5b2eee9548
balasakthi <balaabimanyugnc@gmail.comgit config --global user.name>
2024-05-15 16:47:31 +0530

    routes created

--- bet-app/src/routes/index.js
+++ bet-app/src/routes/index.js
@@ -3,10 +3,6 @@ import { Navigate, useRoutes } from "react-router-dom";
 import Loader from "../pages/Loader/Loader";
 import GuestGuard from "../guards/GuestGaurd";
 import AuthGuard from "../guards/AuthGuard";
-
-
-
-
 const Loadable = (Component) => (props) => {
   return (
     <Suspense fallback={<Loader />}>
@@ -14,70 +10,77 @@ const Loadable = (Component) => (props) => {
     </Suspense>
   );
 };
-
 export default function Router() {
   return useRoutes([
-
     {
-          path: "/",
-          element: (
-            <GuestGuard>
-              <Login />
-            </GuestGuard>
-          ),
-        },
-        
-     
+      path: "/",
+      element: (
+        <GuestGuard>
+          <Login />
+        </GuestGuard>
+      ),
+    },
     {
+      path: "/admin",
+      element: (
+        <AuthGuard>
+          <DashboardLayout />
+        </AuthGuard>
+      ),
+      
       children: [
         {
-          path: "/admin",
+          path: "/admin/user-list",
           element: (
             <AuthGuard>
-              <DashboardLayout/>
+              <UserList />
             </AuthGuard>
           ),
-       
+        },
+        {
+          path: "/admin/post",
+          element: (
+            <AuthGuard>
+              <Post />
+            </AuthGuard>
+          ),
+        },
+        {
+          path: "/admin/dashboard",
+          element: (
+            <AuthGuard>
+              <Dashboard />
+            </AuthGuard>
+          ),
         },
         
       ],
     },
-
-
-
-
+    
     {
-       path: "*",
-       children: [
-         { path: "404", element: <NotFound /> },
-         { path: "*", element: <Navigate to="/404" replace /> },
-       ],
-     },
+      path: "404",
+      element: <NotFound />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/404" replace />,
+    },
   ]);
 }
-
 const Login = Loadable(lazy(() => import("../pages/loginForms/Login")));
 const DashboardLayout = Loadable(lazy(() => import("../pages/Dashboard/DashboardLayout")));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const NotFound = Loadable(lazy(() => import("../pages/404/Page404")));
+const UserList = Loadable(lazy(() => import("../pages/UserList/UserList")));
+const Post = Loadable(lazy(() => import("../pages/ViewPost/Post")));
+const Dashboard = Loadable(lazy(() => import("../pages/Dashboard/Dashboard")));
+
+
+
+
+
+
+
+
+
+
+
