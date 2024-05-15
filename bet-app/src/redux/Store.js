54a3f1c9f0a3268c65526f754173b18ace40f808
balasakthi <balaabimanyugnc@gmail.comgit config --global user.name>
2024-05-15 16:48:00 +0530

    making some changes

--- bet-app/src/redux/Store.js
+++ bet-app/src/redux/Store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "../redux/userSlice";
 import { AuthApi } from "./api/AuthApi";
+import { UserListApi } from "./api/UserListApi";
+import { PostApi } from "./api/PostApi";
+import { DashboardApi } from "./api/DashboardApi";
+
+
 
 
 
@@ -9,12 +14,18 @@ export const store = configureStore({
   reducer: {
     User: UserReducer,
     [AuthApi.reducerPath]: AuthApi.reducer,
+    [UserListApi.reducerPath]:UserListApi.reducer,
+    [PostApi.reducerPath]:PostApi.reducer,
+    [DashboardApi.reducerPath]:DashboardApi.reducer,
   
   },
   devTools: process.env.NODE_ENV === "development",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       AuthApi.middleware,
+      UserListApi.middleware,
+      PostApi.middleware,
+      DashboardApi.middleware,
 
     ]),
 });
