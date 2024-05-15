d2faa6d3c62ecd750d3ba644c497f525195a1335
balasakthi <balaabimanyugnc@gmail.comgit config --global user.name>
2024-05-15 18:32:07 +0530

    side bar menu created for isse , feedback pages

--- bet-app/src/pages/Navbars/SideMenu_Data.js
+++ bet-app/src/pages/Navbars/SideMenu_Data.js
@@ -1,7 +1,8 @@
 
-import { MdAdminPanelSettings } from 'react-icons/md';
+import { MdAdminPanelSettings, MdFeedback } from 'react-icons/md';
 import { FiUser  } from 'react-icons/fi';
 import { FaStreetView } from "react-icons/fa";
+import { AiOutlineIssuesClose } from 'react-icons/ai';
 
 
 
@@ -25,7 +26,23 @@ export const sidebarItems = [
     "url": "/admin/user-list",
   },
   {
-    "id": 2,
+    "id": 3,
+    "label": "Issues",
+    "parent_id": null,
+    "icon":<AiOutlineIssuesClose   size={20}/>,
+    "order_index": 8,
+    "url": "/admin/issue",
+  },
+  {
+    "id": 4,
+    "label": "FeedBack",
+    "parent_id": null,
+    "icon":<MdFeedback   size={20}/>,
+    "order_index": 8,
+    "url": "/admin/feedback",
+  },
+  {
+    "id": 5,
     "label": "Post",
     "parent_id": null,
     "icon":<FaStreetView  size={20}/>,
